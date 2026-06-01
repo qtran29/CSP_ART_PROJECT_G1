b7c0765efd79694b493267b16c5e615534976faa
thnguyen29 <thnguyen29@ssis.edu.vn>
2026-06-01 08:25:59 +0700

    added function call

--- my_picture.py
+++ my_picture.py
@@ -36,22 +36,12 @@ def draw_picture(width, height):
     
     #Right side of the court (sonny)
     
+    #Sonny's changes function
     #Draw 3 point line
-    sg.set_line_thickness(2)
-    sg.set_outline_color("white")
-    sg.draw_line(500,40,600,40)
-    sg.draw_line(500,360,600,360)
-    sg.draw_curve( [(500,40), (300,200), (500,360)] )
+    sg.three_point_line("white")
     
-    #inner paint
-    sg.set_fill_color("#DC8535")
-    sg.fill_rectangle(460, 160, 200, 80)
-    
-    #Outer paint
-    sg.draw_line(460,140,460,260)
-    sg.draw_line(460,140,600,140)
-    sg.draw_line(460,260,600,260)
-    
+    #Paint
+    sg.paint()
     #freethrow circle
     sg.draw_circle(460, 200, 40, 2)
     
